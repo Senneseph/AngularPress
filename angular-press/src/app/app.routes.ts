fa2e38ca1f780bfe578b800362ce52543d93da51
Benjamin Hill <senneseph@gmail.com>
2025-11-09 18:12:54 -0500

    Creating routes

--- angular-press/src/app/app.routes.ts
+++ angular-press/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', loadComponent: () => import('./components/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+  { path: 'posts', loadComponent: () => import('./components/posts/posts.component').then(m => m.PostsComponent) },
+  { path: 'posts/new', loadComponent: () => import('./components/posts/post-form/post-form.component').then(m => m.PostFormComponent) },
+  { path: 'posts/:id', loadComponent: () => import('./components/posts/post-detail/post-detail.component').then(m => m.PostDetailComponent) },
+  { path: 'users', loadComponent: () => import('./components/users/users.component').then(m => m.UsersComponent) },
+  { path: 'categories', loadComponent: () => import('./components/categories/categories.component').then(m => m.CategoriesComponent) },
+  { path: 'tags', loadComponent: () => import('./components/tags/tags.component').then(m => m.TagsComponent) },
+  { path: '**', redirectTo: '/dashboard' }
+];
